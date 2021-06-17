52aa56b74ab8d334c7948f077a3f3afb502c80d1
ProjektKris <rbxkrisxyz@gmail.com>
2021-06-17 22:44:18 +0700

    changed from guessing by longitude to getting countryCode

--- src/shared/performance_stats/getServerRegion.lua
+++ src/shared/performance_stats/getServerRegion.lua
@@ -2,21 +2,23 @@ local HttpService = game:GetService("HttpService")
 local RunService = game:GetService("RunService")
 
 local function getRegion()
-	local longitude = HttpService:JSONDecode(HttpService:GetAsync("http://ip-api.com/json/")).lon
-	local host = "NA" -- north america is default
+	local data = HttpService:JSONDecode(HttpService:GetAsync("http://ip-api.com/json/"))
+	return data.countryCode
+	-- local longitude = HttpService:JSONDecode(HttpService:GetAsync("http://ip-api.com/json/")).lon
+	-- local host = "NA" -- north america is default
 
-	if longitude > -180 and longitude <= -105 then
-		host = "WUS" -- US-West
-	elseif longitude > -105 and longitude <= -90 then
-		host = "CUS" -- US-Central
-	elseif longitude > -90 and longitude <= 0 then
-		host = "EUS" -- US-Eastern
-	elseif longitude <= 75 and longitude > 0 then
-		host = "EU" -- europe
-	elseif longitude <= 180 and longitude > 75 then
-		host = "AS" -- asia
-	end
-	return host
+	-- if longitude > -180 and longitude <= -105 then
+	-- 	host = "WUS" -- US-West
+	-- elseif longitude > -105 and longitude <= -90 then
+	-- 	host = "CUS" -- US-Central
+	-- elseif longitude > -90 and longitude <= 0 then
+	-- 	host = "EUS" -- US-Eastern
+	-- elseif longitude <= 75 and longitude > 0 then
+	-- 	host = "EU" -- europe
+	-- elseif longitude <= 180 and longitude > 75 then
+	-- 	host = "AS" -- asia
+	-- end
+	-- return host
 end
 
 return function()
